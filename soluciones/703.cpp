432f560822791cd31f160b6f3f003cab12d4b9aa
vadik317 <111152105+vadik317@users.noreply.github.com>
2024-01-05 18:40:25 +0000

    Create 703.cpp

---
+++ soluciones/703.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int casos;
+    int horas,minutos,velocidad;
+    cin >> casos;
+    for(int i; i < casos; i++) {
+    scanf("%d:%d 0 %d",&horas,&minutos,&velocidad);
+    cout << ((horas + (minutos / 60.0))*velocidad)<<"\n";
+    }
+}
