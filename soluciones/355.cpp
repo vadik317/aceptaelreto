a4007544f0d71da0b0ace4ef6a4bf6920a219eee
vadik317 <111152105+vadik317@users.noreply.github.com>
2024-01-05 18:39:04 +0000

    Create 355.cpp

---
+++ soluciones/355.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dias, casos;
+    cin >> casos;
+    while (casos) {
+        casos--;
+        cin >> dias;
+        if (dias%4 == 0 && dias%100 == 0 && dias %400 == 0){
+            cout << "29\n";
+        } else if (dias%4 == 0 && dias % 100 != 0) {
+            cout << "29\n";
+        } else {
+            cout << "28\n"; 
+    }
+    }
+}
