ad45d81c5d2170bfefae65ad58e749a460e5ba42
vadik317 <111152105+vadik317@users.noreply.github.com>
2024-01-10 21:24:56 +0000

    Update 621.cpp

--- soluciones/621.cpp
+++ soluciones/621.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int casos,num;
+    cin >> casos;
+    while(casos--){
+        cin >> num;
+        if (num%2 == 0) cout << num+1 << "\n";
+        else {
+            cout << num-1 << "\n";
+        }
+    }
+    return 0;
+}
