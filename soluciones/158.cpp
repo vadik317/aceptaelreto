f42e99d2f95089bd3532551faea123d718e72bf5
vadik317 <111152105+vadik317@users.noreply.github.com>
2024-01-11 19:03:32 +0000

    Create 158.cpp

---
+++ soluciones/158.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int casos,saltos,actual,anterior,arriba,abajo;
+    cin >> casos;
+    while (casos--){
+        cin >> saltos;
+        saltos--;
+        cin >> anterior;
+        while(saltos--){
+            cin >> actual;
+            if (anterior < actual) arriba++;
+            else if (anterior > actual) abajo++;
+            anterior = actual;
+        }
+        cout << arriba << " " << abajo << "\n";
+        arriba = 0;
+        abajo = 0;
+    }
+}
